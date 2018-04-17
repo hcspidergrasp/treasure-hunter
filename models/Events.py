398b5b71405ce9f991c48553492b8b0882521c2c
hcpsidergrasp <hcspidergrasp@gmail.com>
2018-04-17 23:51:57 +0300

    Created events for the game

---
+++ models/Events.py
@@ -0,0 +1,23 @@
+class Event:
+
+    def __init__(self):
+        # Prison
+        self.metGopnik = false
+        self.keyFound = false
+        self.gopnikKilled = false
+        self.robedGopnik = false
+        self.returnedMoney = false
+
+        # Guardian Room
+        self.metGuardian = false
+        self.guardianKilled = false
+        self.robedGuardian = false
+        
+        # Yard
+        self.tauntedPigeons = false
+        self.fedPigeons = false
+        
+        # Dragon Room
+        self.metDragon = false
+        self.wonDragon = false
+        self.loseToDragon = false
